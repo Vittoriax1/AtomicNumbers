633b2cdd3946583320da0bec9572a5b77e9c93aa
TechniGRL.com <99565350+Vittoriax1@users.noreply.github.com>
2023-07-25 18:10:18 -0400

    Add files via upload

---
+++ AtomicNumbersCalc.py
@@ -0,0 +1,38 @@
+# Dictionary mapping atomic symbols to atomic numbers
+atomic_numbers = {
+    'H': 1, 'He': 2, 'Li': 3, 'Be': 4, 'B': 5, 'C': 6, 'N': 7, 'O': 8, 'F': 9, 'Ne': 10,
+    'Na': 11, 'Mg': 12, 'Al': 13, 'Si': 14, 'P': 15, 'S': 16, 'Cl': 17, 'Ar': 18, 'K': 19, 'Ca': 20,
+    'Sc': 21, 'Ti': 22, 'V': 23, 'Cr': 24, 'Mn': 25, 'Fe': 26, 'Ni': 28, 'Co': 27, 'Cu': 29, 'Zn': 30,
+    'Ga': 31, 'Ge': 32, 'As': 33, 'Se': 34, 'Br': 35, 'Kr': 36, 'Rb': 37, 'Sr': 38, 'Y': 39, 'Zr': 40,
+    'Nb': 41, 'Mo': 42, 'Tc': 43, 'Ru': 44, 'Rh': 45, 'Pd': 46, 'Ag': 47, 'Cd': 48, 'In': 49, 'Sn': 50,
+    'Sb': 51, 'Te': 52, 'I': 53, 'Xe': 54, 'Cs': 55, 'Ba': 56, 'La': 57, 'Hf': 72, 'Ta': 73, 'W': 74,
+    'Re': 75, 'Os': 76, 'Ir': 77, 'Pt': 78, 'Au': 79, 'Hg': 80, 'Tl': 81, 'Pb': 82, 'Bi': 83, 'Th': 90,
+    'Pa': 91, 'U': 92, 'Np': 93, 'Pu': 94, 'Am': 95, 'Cm': 96, 'Bk': 97, 'Cf': 98, 'Es': 99, 'Fm': 100,
+    'Md': 101, 'No': 102, 'Lr': 103, 'Rf': 104, 'Db': 105, 'Sg': 106, 'Bh': 107, 'Hs': 108, 'Mt': 109,
+    'Ds': 110, 'Rg': 111, 'Cn': 112, 'Nh': 113, 'Fl': 114, 'Mc': 115, 'Lv': 116, 'Ts': 117, 'Og': 118,
+}
+
+def calculate_atomic_sum(elements):
+    total_sum = 0
+    atomic_symbols = []
+    for element in elements:
+        if element in atomic_numbers:
+            atomic_sum = atomic_numbers[element]
+            total_sum += atomic_sum
+            atomic_symbols.append(f"{element} ({atomic_sum})")
+        else:
+            print(f"Invalid element symbol: {element}")
+    return total_sum, atomic_symbols
+
+def main():
+    input_elements = input("Enter the atomic symbols separated by spaces: ").split()
+    total_sum, atomic_symbols = calculate_atomic_sum(input_elements)
+    
+    if atomic_symbols:
+        equation = " + ".join(atomic_symbols)
+        print(f"{equation} = {total_sum}")
+    else:
+        print("No valid atomic symbols were entered.")
+
+if __name__ == "__main__":
+    main()
